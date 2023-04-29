b073dc53cf8375f534c49973773c55b49893bbf1
Blue Peak High School <bpstu@tooeleschools.org>
2023-04-29 16:25:03 -0600

    asldkfhads

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -19,6 +19,14 @@ public class GoalScript : MonoBehaviour
     public IEnumerator winState()
     {
         GameManager.instance.selecting = true;
+        GameObject[] objects = Resources.LoadAll<GameObject>("");
+
+        for (int i = 0; i < PlayerManagerScript.playerManager.playerList.Count; i++)
+        {
+            GameObject x = Instantiate(objects[Random.Range(0, objects.Length - 1)]);
+            x.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
+        }
+
         finishedWinAnim = true;
         hasCursors = true;
         while (!finishedWinAnim)
