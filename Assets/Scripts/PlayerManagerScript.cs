5d291334e1a1229cc6e7e9cf74038f566f46b05e
unknown <sethdsquires@gmail.com>
2023-04-29 11:59:42 -0600

    Seth Squires

--- Assets/Scripts/PlayerManagerScript.cs
+++ Assets/Scripts/PlayerManagerScript.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class PlayerManagerScript : MonoBehaviour
 {
+    public List<PlayerScript> playerList;
+    public static PlayerManagerScript playerManager;
+    public PlayerInputManager playerInputManager;
+    public Color[] playerColors;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(playerManager == null)
+        {
+            playerManager = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void onPlayerJoin(PlayerInput player)
     {
-        
+        DontDestroyOnLoad(player);
+        if (player != null && player.tag == "Player")
+        {
+            Debug.Log("player joined");
+           
+            playerList.Add(player.GetComponent<PlayerScript>());
+            
+            player.GetComponentInChildren<SpriteRenderer>().color = playerColors[playerList.Count - 1];
+            }
+            
+        }
+
     }
-}
