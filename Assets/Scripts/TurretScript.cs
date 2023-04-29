26d929e3084559d199494fd6f10331df14fb5c04
unknown <sethdsquires@gmail.com>
2023-04-29 08:04:33 -0600

    Seth Squires

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -6,15 +6,18 @@ public class TurretScript : MonoBehaviour
 {
 
     public Transform target;
-    public int turnMin;
-    public int turnMax;
+    public float turnMin;
+    public float turnMax;
     public float turnSpeed;
     public int bulletSpeed;
+    public BulletScript bulletPrefab;
+    public bool canFire;
+    public float fireRate;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(fire());
     }
 
     // Update is called once per frame
@@ -26,12 +29,35 @@ public class TurretScript : MonoBehaviour
     private void turn()
     {
         // Rotate the turret
+        canFire = true;
         Vector3 direction = (target.position - transform.position);
-        /*Quaternion toRotation = Quaternion.FromToRotation(transform.position, direction); // instead of LookRotation( )
-        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime * turnSpeed);*/
-
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
         Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        Debug.Log(rotation.z);
+        if (rotation.z < turnMin)
+        {
+            rotation = Quaternion.Euler(new Vector3(0, 0, turnMin));
+            canFire = false;
+        }
+        else if (rotation.z > turnMax)
+        {
+            rotation = Quaternion.Euler(new Vector3(0, 0, turnMin));
+            canFire = false;
+        }
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator fire()
+    {
+        while (true)
+        {
+            if (canFire)
+            {
+                BulletScript bullet = Instantiate(bulletPrefab);
+                bullet.launch(transform.position, Mathf.Deg2Rad * transform.rotation.eulerAngles.z, bulletSpeed);
+                yield return new WaitForSeconds(fireRate);
+            }
+
+        }
     }
 }
